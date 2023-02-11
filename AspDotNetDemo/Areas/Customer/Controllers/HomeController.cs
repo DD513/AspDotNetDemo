90be40373d8ddd7dd7dc5d4f2cd57fa4f8578a73
EddyTeng <space890063@gmail.com>
2023-02-11 13:51:31 +0800

    Details UI

--- AspDotNetDemo/Areas/Customer/Controllers/HomeController.cs
+++ AspDotNetDemo/Areas/Customer/Controllers/HomeController.cs
@@ -1,22 +1,38 @@
-﻿using AspDotNetDemo.Models;
+﻿using AspDotNetDemo.DataAccess.Repository.IRepository;
+using AspDotNetDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace AspDotNetDemo.Areas.Customer.Controllers
 {
-    [Area("Customer")] // 本次新增程式碼
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        // 本次新增程式碼
+        public IActionResult Details(int productId)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == productId, includeProperties: "Category"),
+
+            };
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
