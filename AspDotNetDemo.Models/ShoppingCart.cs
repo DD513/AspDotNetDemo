d2e531cfc795fb3e91b418952dfd35a47ac57f31
chu fei <900407lisa@gmail.com>
2023-03-06 15:13:20 +0800

    feat: :sparkles: fix detail page style

--- AspDotNetDemo.Models/ShoppingCart.cs
+++ AspDotNetDemo.Models/ShoppingCart.cs
@@ -12,5 +12,8 @@ namespace AspDotNetDemo.Models
         public Product Product { get; set; }
         [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
+        public string Ice { get; set; }
+        public string sweetness { get; set; }
+        
     }
 }
